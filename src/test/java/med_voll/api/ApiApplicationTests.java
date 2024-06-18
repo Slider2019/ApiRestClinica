534674bcad1cfde78d1aba9d78debae234c1ae30
Diego Rojas <diego_rojas87@hotmail.com>
2024-06-17 20:15:17 -0400

    Initial commit

---
+++ src/test/java/med_voll/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package med_voll.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
