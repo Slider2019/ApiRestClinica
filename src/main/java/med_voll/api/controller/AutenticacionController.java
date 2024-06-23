1d039f369b48810c5f16ab50e2da959c2611f876
Diego Rojas <diego_rojas87@hotmail.com>
2024-06-22 20:12:34 -0400

    Segundo commit

--- src/main/java/med_voll/api/controller/AutenticacionController.java
+++ src/main/java/med_voll/api/controller/AutenticacionController.java
@@ -2,6 +2,9 @@ package med_voll.api.controller;
 
 import jakarta.validation.Valid;
 import med_voll.api.domain.usuarios.DatosAutenticacionUsuario;
+import med_voll.api.domain.usuarios.Usuario;
+import med_voll.api.infra.security.DatosJWTtoken;
+import med_voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,12 +22,16 @@ public class AutenticacionController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario) {
-        Authentication token = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(),
+        Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(),
                 datosAutenticacionUsuario.clave());
-        authenticationManager.authenticate(token);
-        return ResponseEntity.ok().build();
+        var usuarioAutenticado = authenticationManager.authenticate(authToken);
+        var JWTtoken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
+        return ResponseEntity.ok(new DatosJWTtoken(JWTtoken));
 
     }
 }
---
+++ src/main/java/med_voll/api/infra/security/DatosJWTtoken.java
@@ -0,0 +1,5 @@
+package med_voll.api.infra.security;
+
+public record DatosJWTtoken(String jwTtoken) {
+
+}
